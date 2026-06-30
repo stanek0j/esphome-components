630c171e88f1096281a3df933522ed242bb36995
Jaromír Staněk <jaromir.stanek@gmail.com>
2026-07-01 01:56:33 +0200

    Fixed include header.

--- components/rubicson/rubicson.h
+++ components/rubicson/rubicson.h
@@ -3,7 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
-#include "esphome/components/remote_base/remote_receiver.h"
+#include "esphome/components/remote_receiver/remote_receiver.h"
 
 #include <vector>
 
