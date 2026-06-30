005bd058090f81aceb875a3ad4e74c1651713c05
Jaromír Staněk <jaromir.stanek@gmail.com>
2026-07-01 01:53:09 +0200

    Fixed missing receiver id.

--- components/rubicson/__init__.py
+++ components/rubicson/__init__.py
@@ -9,4 +9,11 @@ RubicsonComponent = rubicson_ns.class_("RubicsonComponent", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(RubicsonComponent),
+    cv.Required("remote_receiver_id"): cv.use_id(remote_receiver.RemoteReceiverComponent),
 }).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    rr = await cg.get_variable(config["remote_receiver_id"])
+    cg.add(var.set_remote_receiver(rr))
+    await cg.register_component(var, config)
